d76c881dc4367a8d5d4a677fbbf09f63011b574f
Prash0805 <prashantnavratanbothra@gmail.com>
2025-08-02 14:33:18 +0530

    delete

--- src/cpp/macd_strategy.h
+++
@@ -1,9 +0,0 @@
-#ifndef MACD_STRATEGY_H
-#define MACD_STRATEGY_H
-
-#include "data_types.h"
-#include <vector>
-
-std::vector<TradeSignal> compute_macd_signals(const std::vector<OHLCV>& data);
-
-#endif
